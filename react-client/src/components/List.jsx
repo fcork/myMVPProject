f018bb89e0417b8670177cbf612dc9321ee07161
frank <frankcorkery@gmail.com>
2018-07-14 15:04:39 -0400

    Implemented photo on Click -got click fully functional

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
+const List = (props) => {
+  var image = props.jokes.showImage ? <img src="https://www.fanxsaltlake.com/wp-content/uploads/2018/04/Delta-Force.png" /> : null
+    
+  return (
   <div>
     <h4> Joke Counter </h4>
-    There are { props.jokes.length } jokes.
-    { props.jokes.map((joke) => {
+    There are { props.jokes.jokes.length } jokes.
+
+    <p>{props.jokes.joke}</p>
+    {image}
+
+
+
+    {/* { props.jokes.map((joke, i) => {
+      
+      if (i === randomIndex)
       return (<li>{joke.joke}</li>)
-      })}
+      })} */}
   </div>
-)
+  )
+}
+var randomIndex = Math.floor(Math.random() * 558)
+
 
 export default List;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,7 +8,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      jokes: []
+      jokes: [],
+      joke:  '',
+      showImage: false
     }
     this.getJoke = this.getJoke.bind(this)
   }
@@ -27,19 +29,25 @@ class App extends React.Component {
     // });
     axios.get('/jokes/')
     .then((data) => {
-      console.log('got to client', data.data.value[0].joke)
+      console.log('got to client', data.data.value)
       this.setState({jokes: data.data.value})
     })
   }
-
+  
   getJoke(){
-    return <p>{this.state.jokes}</p>
+    var randomIndex = Math.floor(Math.random() * 558)
+    this.setState({joke: this.state.jokes[randomIndex].joke, showImage: !this.state.showImage})
   }
+
+  // addImage(){
+
+  //   this.setState({img: 'https://twt-thumbs.washtimes.com/media/image/2015/03/24/Chuck_Norris_Facebook_s2048x2045.jpg?af9380512ad67281ef1f71f50db400b3288ff981'})
+  // }
 
   render () {
     return (<div>
       <h1>Chuck Norris Random Joke Generator</h1>
-      <List jokes={this.state.jokes}/>
+      <List jokes={this.state}/>
       <button onClick={this.getJoke}>I'm Ready To Laugh</button>
       <button>I'm Not Ready To Laugh</button>
     </div>)
